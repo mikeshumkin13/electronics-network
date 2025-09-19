e33b46cf298767d3d4cbd3deca1b0a9f0cad5270
a12345 <shu.m2010@yandex.ru>
2025-09-19 15:30:49 +0300

    feat(api): CRUD для NetworkNode + фильтрация по стране

--- network/urls.py
+++ network/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductViewSet
+from .views import NetworkNodeViewSet, ProductViewSet
 
 router = DefaultRouter()
 router.register(r"products", ProductViewSet, basename="product")
+router.register(r"network-nodes", NetworkNodeViewSet, basename="networknode")
 
 urlpatterns = router.urls
--- network/views.py
+++ network/views.py
@@ -1,8 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
 
-from .models import Product
+from .models import NetworkNode, Product
 from .permissions import IsActiveStaff
-from .serializers import ProductSerializer
+from .serializers import NetworkNodeSerializer, ProductSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -14,3 +16,20 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsActiveStaff]
+
+
+class NetworkNodeViewSet(viewsets.ModelViewSet):
+    """
+    CRUD по звеньям сети (завод/розница/ИП).
+    Поле debt недоступно к изменению (см. сериализатор).
+    Фильтрация по стране
+    """
+
+    queryset = NetworkNode.objects.select_related("supplier").prefetch_related("products").all()
+    serializer_class = NetworkNodeSerializer
+    permission_classes = [IsActiveStaff]
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = ["country"]
+    ordering_fields = ["name", "level", "city", "country", "created_at"]
+    search_fields = ["name", "city", "country", "email"]
